647a52256632e96f80068cf204257e81f5cd1841
Joseph Adah <jonathanadahjoe@gmail.com>
2019-07-18 17:00:24 +0100

    initial setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h2>Home Page</h2>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
